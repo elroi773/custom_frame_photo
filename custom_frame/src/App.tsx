1a9b4262f1bf1d6b3c7ec0ae91ed0389ce21f9bf
김이레 <165885725+elroi773@users.noreply.github.com>
2026-03-03 11:13:53 +0900

    splash index css 수정

--- custom_frame/src/App.tsx
+++ custom_frame/src/App.tsx
@@ -1,12 +1,20 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Index from "./pages/index";
+import Index from "./pages/Index";
+import Splash from "./pages/Splash";
 
 function App() {
   return (
     <Routes>
-      {/* Default (home) */}
+      {/* Home */}
+      <Route path="/" element={<Splash />} />
+
+      {/* Optional: keep /index working */}
       <Route path="/index" element={<Index />} />
+
+      {/* Fallback: any unknown path -> home */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
+
 export default App;
