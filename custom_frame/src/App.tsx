0fc10bd19e07d8de2a53f4cc628f2c06b451096a
김이레 <165885725+elroi773@users.noreply.github.com>
2026-03-04 08:17:19 +0900

    로그인 회원가입 구현

--- custom_frame/src/App.tsx
+++ custom_frame/src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import Splash from "./pages/Splash";
+import Login from "./pages/Login/Login";
+import Join from "./pages/Join/Join";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
 
       {/* Fallback: any unknown path -> home */}
       <Route path="*" element={<Navigate to="/" replace />} />
+      <Route path="/login" element={<Login />}/>
+      <Route path="/join" element={<Join />}/>
     </Routes>
   );
 }
