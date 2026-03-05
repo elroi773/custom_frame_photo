add9ccb82dedcbe190f8b3d0407611e7d5f8e269
김이레 <165885725+elroi773@users.noreply.github.com>
2026-03-05 19:45:27 +0900

    프레임 커스텀하기

--- custom_frame/src/App.tsx
+++ custom_frame/src/App.tsx
@@ -3,6 +3,10 @@ import Index from "./pages/Index";
 import Splash from "./pages/Splash";
 import Login from "./pages/Login/Login";
 import Join from "./pages/Join/Join";
+import Custom1 from "./pages/custom/Custom1";
+import Photo from "./pages/photo/Photo";
+import Custom2 from "./pages/custom/Custom2";
+import Photo1 from "./pages/custom/Photo1";
 
 function App() {
   return (
@@ -17,6 +21,10 @@ function App() {
       <Route path="*" element={<Navigate to="/" replace />} />
       <Route path="/login" element={<Login />}/>
       <Route path="/join" element={<Join />}/>
+      <Route path="/custom1" element={<Custom1 />}/>
+      <Route path="/custom2" element={<Custom2 />}/>
+      <Route path="/photo1" element={<Photo />}/>
+      <Route path="/customphoto1" element={<Photo1 />}/>
     </Routes>
   );
 }
